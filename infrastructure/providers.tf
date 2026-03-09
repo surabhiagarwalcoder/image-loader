f74f624190a1b1ba01bf7ac045aa102e1fac0b54
Sumeeran Parida <surabhiagarwal70@gmail.com>
2026-03-09 17:01:34 +0530

    change s3 and migrate

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "image-platform-terraform-state-835826344908"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
